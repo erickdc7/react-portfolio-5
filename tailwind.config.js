1a95a2ed3360f98e1effd0d2c98c1f8a9ab27d79
ErickDC <erickdiazc717@gmail.com>
2023-05-28 12:01:04 -0500

    fontFamily added in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,10 @@ module.exports = {
         "gradient-rainbow": "linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)",
         "gradient-rainblue": "linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)",
       }),
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+        opensans: ["Open Sans", "sans-serif"]
+      },
     },
   },
   plugins: [],
