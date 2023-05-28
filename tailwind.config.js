10a95ab5930d1dfc79eb1578a48e014cffe0a4ac
ErickDC <erickdiazc717@gmail.com>
2023-05-28 11:54:18 -0500

    colors added in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
   mode: 'jit',
   theme: {
     extend: {
-      
+      colors: {
+        blue: "#2CBCE9",
+        red: "#DC4492",
+        yellow: "#FDCC49",
+        grey: "#EDEDED",
+        "deep-blue": "#010026",
+        "dark-grey": "#757575",
+        "opaque-black": "rgba(0, 0, 0, 0.35)",
+      }
     },
   },
   plugins: [],
