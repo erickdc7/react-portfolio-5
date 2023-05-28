b2bbf2c45c272a6c9c66912557092ae02e2ce028
ErickDC <erickdiazc717@gmail.com>
2023-05-28 11:53:58 -0500

    tailwind.config.js edited

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx}"],
+  mode: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
   plugins: [],
 }
