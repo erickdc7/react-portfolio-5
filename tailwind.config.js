d1abb87da7d8b55fa0b86682114a96ce290f8931
ErickDC <erickdiazc717@gmail.com>
2023-05-28 12:03:04 -0500

    content added in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,12 @@ module.exports = {
         playfair: ["Playfair Display", "serif"],
         opensans: ["Open Sans", "sans-serif"]
       },
+      content: {
+        brush: "url('./assets/brush.png')",
+        person1: "url('./assets/person-1.png')",
+        person2: "url('./assets/person-2.png')",
+        person3: "url('./assets/person-3.png')",
+      }
     },
   },
   plugins: [],
