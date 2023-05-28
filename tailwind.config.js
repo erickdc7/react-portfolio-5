e6e9908db436026a5e98b4dbabf8906d8f975509
ErickDC <erickdiazc717@gmail.com>
2023-05-28 12:00:31 -0500

    backgroundImage added in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,11 @@ module.exports = {
         "deep-blue": "#010026",
         "dark-grey": "#757575",
         "opaque-black": "rgba(0, 0, 0, 0.35)",
-      }
+      },
+      backgroundImage: (theme) => ({
+        "gradient-rainbow": "linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)",
+        "gradient-rainblue": "linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)",
+      }),
     },
   },
   plugins: [],
