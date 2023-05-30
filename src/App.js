3fb0371ddc09a055ff4d467ccc4ba22ecf52dfe3
ErickDC <erickdiazc717@gmail.com>
2023-05-30 11:18:55 -0500

    useEffect created in Navbar.jsx

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from "./scenes/Navbar";
 
@@ -7,7 +7,16 @@ function App() {
 	const [isTopOfPage, setIsTopOfPage] = useState(true);
 	const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY === 0) setIsTopOfPage(true);
+			if (window.scrollY !== 0) setIsTopOfPage(false);
+		}
 
+		window.addEventListener("scroll", handleScroll);
+
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
 
 	return (
 		<div className="app bg-deep-blue">
