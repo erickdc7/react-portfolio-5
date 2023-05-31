ec787abe08becffe6ef4237f284e50a8572f695c
ErickDC <erickdiazc717@gmail.com>
2023-05-31 11:52:29 -0500

    const isAboveMediumScreens created in Landing.jsx

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -3,6 +3,8 @@ import { motion } from "framer-motion"
 import AnchorLink from "react-anchor-link-smooth-scroll"
 
 const Landing = ({ setSelectedPage }) => {
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
     return (
         <div>Landing</div>
     )
