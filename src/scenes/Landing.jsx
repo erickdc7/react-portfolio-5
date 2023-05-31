6866a6867f159559a592679a4460338ebd348394
ErickDC <erickdiazc717@gmail.com>
2023-05-31 11:50:58 -0500

    Landing.jsx updated

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import useMediaQuery from "../hooks/useMediaQuery"
+import { motion } from "framer-motion"
+import AnchorLink from "react-anchor-link-smooth-scroll"
 
-const Landing = () => {
+const Landing = ({ setSelectedPage }) => {
     return (
         <div>Landing</div>
     )
