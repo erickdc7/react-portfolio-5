50b647ce5b6c29079858b97030519a3d2aa5ea4d
ErickDC <erickdiazc717@gmail.com>
2023-06-01 11:37:32 -0500

    call to actions added in Landing.jsx

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -45,7 +45,7 @@ const Landing = ({ setSelectedPage }) => {
                 >
                     <p className="text-6xl font-playfair z-10 text-center md:text-start">
                         Jane {""}
-                        <span className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush before:absolute before:-left--[25px] before:-top-[70px] before:z-[-1]">
+                        <span className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]">
                             Esper
                         </span>
                     </p>
@@ -53,6 +53,39 @@ const Landing = ({ setSelectedPage }) => {
                     <p className="mt-10 mb-7 text-sm text-center md:text-start">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis sapiente voluptatibus nemo natus odio repellat molestias repellendus commodi esse at?
                     </p>
+                </motion.div>
+
+                {/* CALL TO ACTIONS */}
+                <motion.div
+                    className="flex mt-5 justify-center md:justify-start"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.2, duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -50 },
+                        visible: { opacity: 1, x: 0 }
+                    }}
+                >
+                    <AnchorLink
+                        className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold hover:bg-blue hover:text-white transition duration-500"
+                        onClick={() => setSelectedPage("contact")}
+                        href="#contact"
+                    >
+                        Contact Me
+                    </AnchorLink>
+
+                    <AnchorLink
+                        className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
+                        onClick={() => setSelectedPage("contact")}
+                        href="#contact"
+                    >
+                        <div
+                            className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center justify-center font-playfair px-10"
+                        >
+                            Let's talk.
+                        </div>
+                    </AnchorLink>
                 </motion.div>
             </div>
         </section>
