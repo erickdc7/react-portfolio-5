49a0703b21d56afa889f5f60c5b1916668b5db84
ErickDC <erickdiazc717@gmail.com>
2023-06-01 11:27:14 -0500

    minor fixes in Landing.jsx

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -28,32 +28,32 @@ const Landing = ({ setSelectedPage }) => {
                             />
                         )
                 }
+            </div>
 
-                {/* MAIN SECTION */}
-                <div className="z-30 basis-2/5 mt-12 md:mt-32">
-                    {/* HEADINGS */}
-                    <motion.div
-                        initial="hidden"
-                        whileInView="visible"
-                        viewport={{ once: true, amount: 0.5 }}
-                        transition={{ duration: 0.5 }}
-                        variants={{
-                            hidden: { opacity: 0, x: -50 },
-                            visible: { opacity: 1, x: 0 }
-                        }}
-                    >
-                        <p className="text-6xl font-playfair z-10 text-center md:text-start">
-                            Jane {""}
-                            <span className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush before:absolute before:-left--[25px] before:-top-[70px] before:z-[-1]">
-                                Esper
-                            </span>
-                        </p>
+            {/* MAIN SECTION */}
+            <div className="z-30 basis-2/5 mt-12 md:mt-32">
+                {/* HEADINGS */}
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -50 },
+                        visible: { opacity: 1, x: 0 }
+                    }}
+                >
+                    <p className="text-6xl font-playfair z-10 text-center md:text-start">
+                        Jane {""}
+                        <span className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush before:absolute before:-left--[25px] before:-top-[70px] before:z-[-1]">
+                            Esper
+                        </span>
+                    </p>
 
-                        <p className="mt-10 mb-7 text-sm text-center md:text-start">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis sapiente voluptatibus nemo natus odio repellat molestias repellendus commodi esse at?
-                        </p>
-                    </motion.div>
-                </div>
+                    <p className="mt-10 mb-7 text-sm text-center md:text-start">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis sapiente voluptatibus nemo natus odio repellat molestias repellendus commodi esse at?
+                    </p>
+                </motion.div>
             </div>
         </section>
     )
