9b235ce59ec546ceac28eddfbe7b8142149a3a0d
ErickDC <erickdiazc717@gmail.com>
2023-06-02 11:35:42 -0500

    LineGradient.jsx updated

--- src/components/LineGradient.jsx
+++ src/components/LineGradient.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-
-const LineGradient = () => {
+const LineGradient = ({ width = "w-full" }) => {
     return (
-        <div>LineGradient</div>
+        <div className={`h-0.5 ${width} bg-gradient-rainblue`} />
     )
 }
 
