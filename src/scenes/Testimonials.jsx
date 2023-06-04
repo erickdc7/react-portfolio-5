3415dd9ced21ad33b173b4883b5522945ebc6034
ErickDC <erickdiazc717@gmail.com>
2023-06-04 11:10:51 -0500

    heading added

--- src/scenes/Testimonials.jsx
+++ src/scenes/Testimonials.jsx
@@ -6,7 +6,28 @@ const Testimonials = () => {
 
     return (
         <section id="testimonials" className="pt-32 pb-16">
+            {/* HEADING */}
+            <motion.div
+                className="md:w-1/3 text-center md:text-left"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ duration: 0.5 }}
+                variants={{
+                    hidden: { opacity: 0, x: -50 },
+                    visible: { opacity: 1, x: 0 }
+                }}
+            >
+                <p className="font-playfair font-semibold text-4xl mb-5 text-red">
+                    TESTIMONIALS
+                </p>
 
+                <LineGradient width="mx-auto w-2/4" />
+
+                <p className="mt-10">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minima maxime nihil neque nemo fuga!
+                </p>
+            </motion.div>
         </section>
     )
 }
