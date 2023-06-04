e2950d3323805e0742d4b2943a3c5c9d79267cfb
ErickDC <erickdiazc717@gmail.com>
2023-06-04 11:07:17 -0500

    Testimonials.jsx updated

--- src/scenes/Testimonials.jsx
+++ src/scenes/Testimonials.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import LineGradient from "../components/LineGradient"
+import { motion } from "framer-motion"
 
 const Testimonials = () => {
+    const testimonialStyles = `mx-auto relative max-w-[400px] h-[350px] flex flex-col justify-end pt-16 mt-48 before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2`;
+
     return (
-        <div>Testimonials</div>
+        <section id="testimonials" className="pt-32 pb-16">
+
+        </section>
     )
 }
 
