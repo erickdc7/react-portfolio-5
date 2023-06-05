94575c90e09558c1416c1653340ae86a64ba0858
ErickDC <erickdiazc717@gmail.com>
2023-06-05 11:43:15 -0500

    Contact.jsx updated

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -3,6 +3,20 @@ import { useForm } from "react-hook-form"
 import { motion } from "framer-motion"
 
 const Contact = () => {
+    const {
+        register,
+        trigger,
+        formState: { errors }
+    } = useForm();
+
+    const onSubmit = async (e) => {
+        const isValid = await trigger();
+
+        if (!isValid) {
+            e.preventDefault();
+        }
+    }
+
     return (
         <div>Contact</div>
     )
