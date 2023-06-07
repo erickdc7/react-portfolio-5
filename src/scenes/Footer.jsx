7bf5b1392088bf56932a23d7b98c4a061f969e43
ErickDC <erickdiazc717@gmail.com>
2023-06-07 12:15:01 -0500

    styles added

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 
                 <div className="md:flex justify-center md:justify-between text-center">
                     <p className="font-playfair font-semibold text-2xl text-yellow">JANE ESPER</p>
-                    <p>©2023 ESPER. All Rigths Reserved.</p>
+                    <p className="font-playfair text-md text-yellow">©2023 ESPER. All Rigths Reserved.</p>
                 </div>
             </div>
         </footer>
