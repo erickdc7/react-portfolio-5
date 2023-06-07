89393a621311875051ae97e9f93ebc0a2ea7832f
ErickDC <erickdiazc717@gmail.com>
2023-06-07 12:12:47 -0500

    Footer.jsx updated

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -3,7 +3,14 @@ import SocialMediaIcons from "../components/SocialMediaIcons"
 const Footer = () => {
     return (
         <footer className="h-64 bg-red pt-10">
-            
+            <div className="w-5/6 mx-auto">
+                <SocialMediaIcons />
+
+                <div>
+                    <p>JANE ESPER</p>
+                    <p>©2023 ESPER. All Rigths Reserved.</p>
+                </div>
+            </div>
         </footer>
     )
 }
