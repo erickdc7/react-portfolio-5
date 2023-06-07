6bcb91bc3c555c1a7fd5b72ae392390075585a1f
ErickDC <erickdiazc717@gmail.com>
2023-06-07 12:11:05 -0500

    styles added

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -2,8 +2,8 @@ import SocialMediaIcons from "../components/SocialMediaIcons"
 
 const Footer = () => {
     return (
-        <footer>
-
+        <footer className="h-64 bg-red pt-10">
+            
         </footer>
     )
 }
